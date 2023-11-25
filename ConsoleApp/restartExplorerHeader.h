bbfd2f06f5b336f87558a21dc6b3291bb80aefa4
ohusq <mennobusiness@proton.me>
2023-11-25 11:22:55 +0100

    Added rock paper scissors and some possible bug fixes.

--- ConsoleApp/restartExplorerHeader.h
+++ ConsoleApp/restartExplorerHeader.h
@@ -1,15 +1,34 @@
 #pragma once
-#include <iostream> // std::cout, std::cerr, std::endl, system()
-#include "taskkill.h" // taskkill_force, restart_task
 
-bool restartExplorer() {
-	int exitCode = restart_task("explorer.exe"); // Restart explorer.exe
-	if (exitCode == 0) { // If explorer.exe was restarted successfully
-			std::cout << "Explorer.exe was restarted successfully!" << std::endl;
-			return true;
-		}
-	else {
-		std::cout << "Failed to restart explorer.exe!" << std::endl;
-		return false;
-	}
+// restarts explorer.exe without using #include <taskkill.h>
+
+#include <iostream>
+#include <windows.h>
+
+int restartExplorer() {
+    // Kill the explorer.exe thread and respawn one.
+    const char* commandToKill{ "taskkill /f /im explorer.exe" };
+    const char* commandToRestart{ "start explorer.exe" };
+
+    int killExplorer{ system(commandToKill) };
+
+    if (killExplorer == 0) {
+        std::cout << "Succesfully killed explorer.exe\nRestarting..." << std::endl;
+    }
+    else {
+        std::cout << "Failed to kill explorer.exe!\nStopping..." << std::endl;
+        exit(0);
+    }
+    
+    int startExplorer{ system(commandToRestart) };
+
+    if (startExplorer == 0) {
+        std::cout << "Restarted explorer.exe succesfully" << std::endl;
+    }
+    else {
+        std::cout << "Failed to restart explorer.exe!\nPlease start explorer.exe manually" << std::endl;
+        Sleep(sizeof(DWORD));
+    }
+
+    return 0;
 }
--- ConsoleApp/taskkill.h
+++ ConsoleApp/taskkill.h
@@ -2,15 +2,15 @@
 #include <iostream>
 
 int taskkill_force(const std::string& processName) {
-	std::string command = "taskkill /f /im " + processName;
+	std::string command{ "taskkill /f /im " + processName };
 	return system(command.c_str()); // Execute command and return exit code (0 = success, 1 = error)
 }
 
 int restart_task(const std::string& processName) {
-	int exitCode = taskkill_force(processName); // Kill process
+	int exitCode{ taskkill_force(processName) }; // Kill process
 	if (exitCode == 0) { // If process was killed successfully
 			std::cout << "Process " << processName << " was killed successfully!" << std::endl;
-			std::cout << "Restarting process... (if you're stuck on here its finished)" << std::endl;
+			std::cout << "Restarting process... (if you're stuck on here its a bug)" << std::endl;
 			return system(processName.c_str()); // Restart process
 		}
 	else {
